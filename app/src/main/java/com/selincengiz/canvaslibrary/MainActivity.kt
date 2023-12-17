06abb64313e1f45a98d80eb116aa6974f3c05268
Selin Ayten Cengiz <selincengiz41@gmail.com>
2023-12-18 00:08:42 +0300

    maven eklendi

--- app/src/main/java/com/selincengiz/canvaslibrary/MainActivity.kt
+++ app/src/main/java/com/selincengiz/canvaslibrary/MainActivity.kt
@@ -2,10 +2,13 @@ package com.selincengiz.canvaslibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.selincengiz.paint_view.PaintView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
